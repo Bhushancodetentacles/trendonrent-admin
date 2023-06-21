d2a641c15fec468e00dfec2ae016ca540241bb2e
RuchitaSonar <118338252+RuchitaSonar@users.noreply.github.com>
2023-06-21 19:08:44 +0530

    changes

--- src/pages/Policy/TermsAndConditions.js
+++ src/pages/Policy/TermsAndConditions.js
@@ -2,9 +2,35 @@ import React from 'react'
 
 function TermsAndConditions() {
   return (
-    <div>
-      TermsAndConditions
+    <section>
+    <div className='container'>
+      <div className="row mx-auto justify-content-center"  >
+        <div className="col-md-8">
+        <div className="policy_page">
+            <h3 className="heading_in2">Terms And Conditions</h3>
+            <ul className='policy_ul'>
+            <li>Minimum Renting Duration: 3 days.
+                    <ul>
+                      <li>First Day - Dress can be picked up anytime after 2:00 pm</li>
+                      <li>Second Day - Highlight day when you'll look amazing and be the center of attraction.</li>
+                      <li>Third Day  - Dress must be returned before 8:30 pm </li>
+                    </ul>
+                  </li>
+                  <li> Renting Packages: We offer 3-day, 7-day and 10-day  packages with pickup and return on the first and last day respectively.</li>
+                  <li>Pickup and Return Timing: For all rentals, pickup after 2:00 pm and return before 8:30 pm.</li>
+                  <li>Delay in Return: Please inform us in advance for any delays. No charge for delays of a few hours, but additional charges for non-return on the designated day.</li>
+                  <li> Non-Refundable Rent Amount: Rent paid is non-refundable</li>
+                  <li>Deposit Amount: A deposit is required to secure the dress. Deposit varies for each dress and is specified during rental. Deposit returned within  3 days of dress return.</li>
+                  <li>Trend on Rent denies liability for losses caused by handover delays and defers all matters to the Pune court</li>
+              
+            </ul>
+        
+                <em className="termsem">By renting a dress, you agree to abide by these terms and conditions.</em>
+          </div>
+        </div>
+      </div>
     </div>
+  </section>
   )
 }
 
